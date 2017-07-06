8c7942bd1acb195916da02717b247944dfaaf959
AhmedShab <ahmed.vuw@gmail.com>
2017-07-07 08:48:35 +1200

    handling errors

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module FinancialApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.financial_app = self.routes
   end
 end
